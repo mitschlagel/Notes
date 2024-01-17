4d3e9fb41a565ae6d44c8504e181ce19574a937c
Spencer Jones <sjonesdrums@gmail.com>
2024-01-16 19:46:30 -0600

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Spencer Jones on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
